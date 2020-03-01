c68f2d3941de7404cccf0aab77ebfd807e7d437e
ryan m <contact@ryanmeek.com>
2020-03-01 18:49:24 +0000

    Done.

--- db/migrate/20150915202704_create_owners.rb
+++ db/migrate/20150915202704_create_owners.rb
@@ -1,11 +1,3 @@
 class CreateOwners < ActiveRecord::Migration
-  def up
-    create_table :owners do |t|
-      t.string :name
-    end
-  end
-  
-  def down
-    drop_table :owners
-  end
+  def change() create_table(:owners) { |t| t.string :name } end
 end
--- db/migrate/20150915202821_add_column_to_cats.rb
+++ db/migrate/20150915202821_add_column_to_cats.rb
@@ -1,9 +1,3 @@
 class AddColumnToCats < ActiveRecord::Migration
-  def up
-    add_column :cats, :owner_id, :integer
-  end
-
-  def down
-    remove_column :cats, :owner_id
-  end
+  def change() add_column :cats, :owner_id, :integer end
 end
